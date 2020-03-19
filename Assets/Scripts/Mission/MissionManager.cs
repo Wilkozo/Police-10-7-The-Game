9df48ba35b58f732ae863da99010297a91f3f5af
Harry <profawesum@gmail.com>
2020-03-20 11:15:17 +1300

    starting work on a mission manager script
    Also a basic stealing car mission

---
+++ Assets/Scripts/Mission/MissionManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//this is a manager class in which holds what to do when a mission starts and when it ends
+public class MissionManager : MonoBehaviour
+{
+
+
+    private static bool missionStarted;
+
+    public static bool missionStart
+    {
+        get
+        {
+            return missionStarted;
+        }
+        set
+        {
+            missionStarted = value;
+        }
+    }
+
+    //call this when a mission ends in success
+    static public void missionEndSuccess() {
+        //add dialogue saying that you completed the mission
+        missionStarted = false;
+
+    }
+
+    //call this when a mission ends in a failure
+    static public void missionEndFailure() {
+        missionStarted = false;
+    }
+
+
+}
---
+++ Assets/Scripts/Mission/StealACar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class StealACar : MonoBehaviour
+{
+
+    //the car that should be stolen
+    public GameObject carToSteal;
+    public Text objectiveText;
+
+    private void Start()
+    {
+        objectiveText.enabled = false;
+    }
+
+
+    //what to do when a trying to start the mission
+    public void enableMission() {
+        //checks to see if the player is already in a mission
+        if (MissionManager.missionStart == false)
+        {
+            //set it so there is a mission
+            MissionManager.missionStart = true;
+            //set the mission text
+            objectiveText.enabled = true;
+        }
+        //if they are then return
+        else {
+            return;
+        }
+    }
+
+    //trigger collider
+    private void OnTriggerEnter(Collider other)
+    {
+        //if the player collides with the building they are taking the car to
+        if (other.gameObject == carToSteal) {
+            //complete the mission
+            MissionManager.missionEndSuccess();
+            objectiveText.enabled = false;
+            //complete the mission
+            //otherwise dont do anything
+        }
+    }
+}
