6667bb2fc62a798eb92f8acf715ce99de0d139c8
Harry <profawesum@gmail.com>
2020-04-02 16:26:17 +1300

    Menu looking nice

---
+++ Assets/Scripts/Menus/MenuSwap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class MenuSwap : MonoBehaviour
+{
+
+    public string levelToLoad;
+    public Image title;
+    public Image controls;
+    public Text titleText;
+
+
+    private void Start()
+    {
+        controls.enabled = false;
+    }
+
+    public void PlayGame() {
+        Application.LoadLevel("levelToLoad");
+    }
+    
+    public void controlsClicke()
+    {
+        titleText.enabled = false;
+        controls.enabled = true;
+        title.enabled = false;
+    }
+
+    public void onQuitClicked() {
+        Application.Quit();
+    }
+
+
+}
